323e5a3f74d0395d4c60f40fb2401f1d62f0aae5
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 14:46:44 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.Domain/GlobalUsing.cs
+++ DotNet8.Architecture_CleanArchitecture.Domain/GlobalUsing.cs
@@ -1,7 +1,4 @@
-﻿using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architecture_CleanArchitecture.Domain
+﻿namespace DotNet8.Architecture_CleanArchitecture.Domain
 {
 	internal class GlobalUsing
 	{
