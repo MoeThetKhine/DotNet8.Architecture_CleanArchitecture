fa6fa1173a820bb5c4361d8044c0574920c77a58
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 16:54:24 +0630

    Feat : Add PageSettingModel

---
+++ DotNet8.Architecture_CleanArchitecture.DTOs/PageSetting/PageSettingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architecture_CleanArchitecture.DTOs.PageSetting
+{
+	internal class PageSettingModel
+	{
+	}
+}
