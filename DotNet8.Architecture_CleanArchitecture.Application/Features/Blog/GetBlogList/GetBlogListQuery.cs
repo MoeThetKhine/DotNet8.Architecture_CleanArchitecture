6e8c1d549b0baea149bfef8ad39eb954cfa58de9
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 14:31:12 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,8 +1,4 @@
-﻿using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogList
+﻿namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogList
 {
 	internal class GetBlogListQuery
 	{
