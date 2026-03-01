4b70b5116f78172ca1e0c5d9eb8aef40ba43e6ea
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 14:32:27 +0630

    Format Code

--- DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
+++ DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogById
+﻿namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogById;
+
+internal class GetBlogByIdQueryHandler
 {
-	internal class GetBlogByIdQueryHandler
-	{
-	}
 }
