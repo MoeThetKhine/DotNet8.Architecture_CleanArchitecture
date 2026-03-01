c322e52372f81ebac6a6cbead5065095a5819587
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 14:19:52 +0630

    Format Code

--- DotNet8.Architecture_CleanArchitecture.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architecture_CleanArchitecture.Presentation/Controllers/Blog/BlogController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Architecture_CleanArchitecture.Presentation.Controllers.Blog
+namespace DotNet8.Architecture_CleanArchitecture.Presentation.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BlogController : ControllerBase
-	{
-	}
 }
