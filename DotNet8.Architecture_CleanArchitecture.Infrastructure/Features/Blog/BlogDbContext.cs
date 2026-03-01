571649751ad7b336fc03fe8d30828727d817dd60
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 14:21:54 +0630

    Format Code

--- DotNet8.Architecture_CleanArchitecture.Infrastructure/Features/Blog/BlogDbContext.cs
+++ DotNet8.Architecture_CleanArchitecture.Infrastructure/Features/Blog/BlogDbContext.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Infrastructure.Features.Blog
+﻿namespace DotNet8.Architecture_CleanArchitecture.Infrastructure.Features.Blog;
+
+internal class BlogDbContext
 {
-	internal class BlogDbContext
-	{
-	}
 }
