7bf6c41c079b2c703868bdce224e7ad2adc23d8a
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-01 17:15:59 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.Utils/Resource/MessageResource.cs
+++ DotNet8.Architecture_CleanArchitecture.Utils/Resource/MessageResource.cs
@@ -1,7 +1,4 @@
-﻿using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architecture_CleanArchitecture.Utils.Resource
+﻿namespace DotNet8.Architecture_CleanArchitecture.Utils.Resource
 {
 	internal class MessageResource
 	{
