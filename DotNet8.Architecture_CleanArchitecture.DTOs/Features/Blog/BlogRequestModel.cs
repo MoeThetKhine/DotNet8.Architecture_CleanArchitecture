e37af642cf1b1cca72c052978e57f11b9ca51126
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-02 15:21:59 +0630

    Feat : Add BlogRequestModel

--- DotNet8.Architecture_CleanArchitecture.DTOs/Features/Blog/BlogRequestModel.cs
+++ DotNet8.Architecture_CleanArchitecture.DTOs/Features/Blog/BlogRequestModel.cs
@@ -2,4 +2,9 @@
 
 public class BlogRequestModel
 {
+	public string BlogTitle { get; set; } = null!;
+
+	public string BlogAuthor { get; set; } = null!;
+
+	public string BlogContent { get; set; } = null!;
 }
