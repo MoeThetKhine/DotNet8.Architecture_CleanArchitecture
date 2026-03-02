385d5c02cbc66e15c9391b1dbdc6e4a55b62297f
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-02 15:33:41 +0630

    Feat : Add BlogModel ToModel in Extension

--- DotNet8.Architecture_CleanArchitecture.Extension/Extension.cs
+++ DotNet8.Architecture_CleanArchitecture.Extension/Extension.cs
@@ -1,5 +1,18 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Extension;
+﻿using DotNet8.Architecture_CleanArchitecture.DTOs.Features.Blog;
+
+namespace DotNet8.Architecture_CleanArchitecture.Extension;
 
 public static class Extension
 {
+	public static BlogModel ToModel(this TblBlog dataModel)
+	{
+		return new BlogModel
+		{
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+			DeleteFlag = dataModel.DeleteFlag
+		};
+	}
 }
