ac41838b00c4f0ec5130ae2fd2c65f9e420d5d25
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-02 15:54:44 +0630

    Feat : Add TblBlog

--- DotNet8.Architecture_CleanArchitecture.DbService/Models/TblBlog.cs
+++ DotNet8.Architecture_CleanArchitecture.DbService/Models/TblBlog.cs
@@ -1,5 +1,17 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.DbService.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class TblBlog
+namespace DotNet8.Architecture_CleanArchitecture.DbService.Models;
+
+public partial class TblBlog
 {
+	[Key]
+	public long BlogId { get; set; }
+
+	public string BlogTitle { get; set; } = null!;
+
+	public string BlogAuthor { get; set; } = null!;
+
+	public string BlogContent { get; set; } = null!;
+
+	public bool DeleteFlag { get; set; }
 }
