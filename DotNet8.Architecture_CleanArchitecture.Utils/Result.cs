3cb446e8d2bf9f348239a6e60b887dba8a69b53b
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-02 14:35:48 +0630

    Feat : Add Result

--- DotNet8.Architecture_CleanArchitecture.Utils/Result.cs
+++ DotNet8.Architecture_CleanArchitecture.Utils/Result.cs
@@ -1,5 +1,11 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Utils;
+﻿using DotNet8.Architecture_CleanArchitecture.Utils.Enums;
 
-public class Result
-{
+namespace DotNet8.Architecture_CleanArchitecture.Utils;
+
+public class Result<T>
+{ 
+	public T Data { get; set; }
+	public string Message {  get; set; }	
+	public bool IsSuccess {  get; set; }
+	public EnumStatusCode StatusCode { get; set; }
 }
