ee1d2c2e5d9417ded3880aed1acb32bbf65a8bf5
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-02 15:48:36 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.DbService/Models/TblBlog.cs
+++ DotNet8.Architecture_CleanArchitecture.DbService/Models/TblBlog.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.DbService.Models
+﻿namespace DotNet8.Architecture_CleanArchitecture.DbService.Models;
+
+internal class TblBlog
 {
-	internal class TblBlog
-	{
-	}
 }
