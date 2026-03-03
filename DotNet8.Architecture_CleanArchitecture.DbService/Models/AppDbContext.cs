c5aabef28d775e794911aeea0e19ac460db71090
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-03 09:11:28 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.DbService/Models/AppDbContext.cs
+++ DotNet8.Architecture_CleanArchitecture.DbService/Models/AppDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.DbService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.Architecture_CleanArchitecture.DbService.Models;
 
 public partial class AppDbContext : DbContext
 {
+	public AppDbContext()
+	{
+	}
+
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+	   : base(options)
+	{
+	}
 
 }
