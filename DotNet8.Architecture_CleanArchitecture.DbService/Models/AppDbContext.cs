311ed84d5ac501630b718e264d56136c28003bd0
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-03 11:20:33 +0630

    Feat : Add TaskCategory

--- DotNet8.Architecture_CleanArchitecture.DbService/Models/AppDbContext.cs
+++ DotNet8.Architecture_CleanArchitecture.DbService/Models/AppDbContext.cs
@@ -23,5 +23,21 @@ public partial class AppDbContext : DbContext
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 		=> optionsBuilder.UseSqlServer("Server=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
 
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<TaskCategory>(entity =>
+		{
+			entity.HasKey(e => e.CategoryId).HasName("PK__TaskCate__19093A2B739DC4B2");
 
-}
+			entity.ToTable("TaskCategory");
+
+			entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+			entity.Property(e => e.CategoryName)
+				.HasMaxLength(100)
+				.IsUnicode(false);
+		});
+	}
+
+
+
+	}
