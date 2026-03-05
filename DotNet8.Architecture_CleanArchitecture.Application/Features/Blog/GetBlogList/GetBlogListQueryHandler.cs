12b60401f4a606315d8c2f30fd6c13230617bc0f
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-05 09:32:25 +0630

    Feat : Add GetBlogListQueryHandler

--- DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,5 +1,40 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogList;
+﻿using DotNet8.Architecture_CleanArchitecture.Domain.Features.Blog;
+using DotNet8.Architecture_CleanArchitecture.DTOs.Features.Blog;
+using DotNet8.Architecture_CleanArchitecture.Utils;
+using DotNet8.Architecture_CleanArchitecture.Utils.Resource;
+using MediatR;
 
-public class GetBlogListQueryHandler
+namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListModelV1>>
 {
+	private readonly IBlogRepository _blogRepository;
+
+	public GetBlogListQueryHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogListModelV1>> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		if(request.pageNo <= 0)
+		{
+			result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageNo);
+			goto result;
+		}
+
+		if(request.pageSize <= 0)
+		{
+			result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageSize);
+			goto result;
+		}
+
+		result = await _blogRepository.GetBlogsAsync(request.pageNo,request.pageSize, cancellationToken);
+	result:
+		return result;
+
+	}
+
 }
