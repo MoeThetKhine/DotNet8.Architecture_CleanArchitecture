d740ebdb1c74ba0b3231e7e3caa94d3e2c47287a
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-06 08:31:28 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,8 +1,4 @@
-﻿using DotNet8.Architecture_CleanArchitecture.DTOs.Features.Blog;
-using DotNet8.Architecture_CleanArchitecture.Utils;
-using MediatR;
-
-namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogById;
+﻿namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.GetBlogById;
 
 public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 {
