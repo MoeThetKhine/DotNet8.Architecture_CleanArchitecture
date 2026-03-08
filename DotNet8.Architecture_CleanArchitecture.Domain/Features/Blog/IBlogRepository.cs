0a77b3c98ea291e41d5eef4a8190c1f0ef70104d
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-08 16:11:55 +0630

    Feat : Add UpdateBlogAsync in IBlogRepository

--- DotNet8.Architecture_CleanArchitecture.Domain/Features/Blog/IBlogRepository.cs
+++ DotNet8.Architecture_CleanArchitecture.Domain/Features/Blog/IBlogRepository.cs
@@ -8,4 +8,5 @@ public interface IBlogRepository
 	Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int page, CancellationToken cancellationToken);
 	Task<Result<BlogModel>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
 	Task<Result<BlogModel>> CreateBlogAsync(BlogRequestModel blogRequestModel, CancellationToken cancellationToken);
+	Task<Result<BlogModel>> UpdateBlogAsync(int id, BlogRequestModel blogRequestModel, CancellationToken cancellationToken);
 }
