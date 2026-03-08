f55c7cedb32c4fd3de7fac471a62052fa222d629
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-08 16:51:43 +0630

    Feat : Add GetBlogsAsync in BlogRepository

--- DotNet8.Architecture_CleanArchitecture.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Architecture_CleanArchitecture.Infrastructure/Features/Blog/BlogRepository.cs
@@ -1,5 +1,29 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Infrastructure.Features.Blog;
+﻿using DotNet8.Architecture_CleanArchitecture.DTOs.Features.Blog;
+using DotNet8.Architecture_CleanArchitecture.Utils;
+using DotNet8.Architecture_CleanArchitecture.Shared;
 
-public class BlogRepository
+namespace DotNet8.Architecture_CleanArchitecture.Infrastructure.Features.Blog;
+
+public class BlogRepository : IBlogRepository
 {
+	private readonly BlogDbContext _dbContext;
+
+	public BlogRepository(BlogDbContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+
+	public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		try
+		{
+			var query = _dbContext.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+			var lst = await query
+				.Paginate(pageNo, pageSize)
+				.ToListAsync(cancellationToken: cancellationToken);
+
+		}
+	}
 }
