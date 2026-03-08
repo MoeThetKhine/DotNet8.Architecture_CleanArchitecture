001dd0bf496dd436d89bbcdd209981b7adf93fb7
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-08 15:32:27 +0630

    Feat : Add PatchBlogCommandHandler

--- DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ DotNet8.Architecture_CleanArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -1,5 +1,31 @@
-﻿namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.PatchBlog;
+﻿using DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.UpdateBlog;
 
-public class PatchBlogCommandHandler
+namespace DotNet8.Architecture_CleanArchitecture.Application.Features.Blog.PatchBlog;
+
+public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogModel>>
 {
+
+	private readonly IBlogRepository _blogRepository;
+
+	public PatchBlogCommandHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogRepository.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
+
+		result:
+		return result;
+	}
+
 }
