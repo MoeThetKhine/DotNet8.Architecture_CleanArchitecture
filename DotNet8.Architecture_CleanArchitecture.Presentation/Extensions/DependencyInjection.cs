60aeb094574c7f32c37723bcd07c6a4284a1ea2c
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-09 20:33:04 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.Presentation/Extensions/DependencyInjection.cs
+++ DotNet8.Architecture_CleanArchitecture.Presentation/Extensions/DependencyInjection.cs
@@ -1,5 +1,11 @@
 ﻿namespace DotNet8.Architecture_CleanArchitecture.Presentation.Extensions;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
+
+	public static IServiceCollection AddDependencyInjection(this  IServiceCollection services, WebApplicationBuilder builder)
+	{
+
+	}
+
 }
