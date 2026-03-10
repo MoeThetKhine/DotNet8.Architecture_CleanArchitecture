d9a2b072417359946758be7014a977dc47d643a5
Moe Thet Khine <soniapoudel03@gmail.com>
2026-03-10 11:43:20 +0630

    -

--- DotNet8.Architecture_CleanArchitecture.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architecture_CleanArchitecture.Presentation/Controllers/Blog/BlogController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.Architecture_CleanArchitecture.Presentation.Controllers.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BlogController : ControllerBase
+public class BlogController : BaseController
 {
+	private readonly IMediator _mediator;
+
+	public BlogController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
 }
